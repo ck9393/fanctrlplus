c3347eceec477719133fb898d6a531a2dc3f2a9e
ck9393 <34255442+ck9393@users.noreply.github.com>
2025-06-05 02:17:46 +0800

    Create template.php

---
+++ include/template.php
@@ -0,0 +1,59 @@
+<?PHP
+$index = intval($_GET['index'] ?? 0);
+?>
+
+<div class="fan-block" style="display:inline-block; width:48%; vertical-align:top;">
+  <input type="hidden" name="#file[<?=$index?>]" value="">
+  <fieldset style="margin:10px; padding:10px; border:1px solid #ccc; position:relative;">
+    <button type="button" onclick="removeFan(this)" style="position:absolute; top:4px; right:4px;">🗑</button>
+    <legend><strong>Fan Settings</strong></legend>
+    <table style="width:100%;">
+      <tr>
+        <td style="width:140px;">Fan Control:</td>
+        <td>
+          <select name="service[<?=$index?>]">
+            <option value="0">Disabled</option>
+            <option value="1">Enabled</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>PWM Controller:</td>
+        <td>
+          <select name="controller[<?=$index?>]">
+            <?php
+            exec("find /sys/devices -type f -iname 'pwm[0-9]' -exec dirname \"{}\" + | uniq", $chips);
+            foreach ($chips as $chip) {
+              $name = is_file("$chip/name") ? file_get_contents("$chip/name") : '';
+              foreach (glob("$chip/pwm[0-9]") as $pwm) {
+                echo "<option value=\"$pwm\">$name - ".basename($pwm)."</option>";
+              }
+            }
+            ?>
+          </select>
+          <button type="button" onclick="pauseFan($(this).prev().val(), this)">Pause 30s</button>
+        </td>
+      </tr>
+      <tr><td>Min PWM:</td><td><input type="text" name="pwm[<?=$index?>]" value="0"></td></tr>
+      <tr><td>Low Temp (°C):</td><td><input type="number" name="low[<?=$index?>]" min="0" max="100" value="35"></td></tr>
+      <tr><td>High Temp (°C):</td><td><input type="number" name="high[<?=$index?>]" min="0" max="100" value="50"></td></tr>
+      <tr><td>Interval (min):</td><td><input type="number" name="interval[<?=$index?>]" min="1" max="60" value="5"></td></tr>
+      <tr>
+        <td>Include Disks:</td>
+        <td>
+          <select class="disk-select" name="disks[<?=$index?>][]" multiple style="width:400px;">
+            <?php
+            foreach (glob("/dev/disk/by-id/*") as $dev) {
+              if (!is_link($dev) || strpos($dev, "part") !== false) continue;
+              $real = realpath($dev);
+              if (strpos($real, "/dev/sd") === false && strpos($real, "/dev/nvme") === false) continue;
+              $id = basename($dev);
+              echo "<option value=\"$id\">$id → $real</option>";
+            }
+            ?>
+          </select>
+        </td>
+      </tr>
+    </table>
+  </fieldset>
+</div>
