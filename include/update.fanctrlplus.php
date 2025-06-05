993463d23b607bff0ec72aed3f35303ebc594902
ck9393 <34255442+ck9393@users.noreply.github.com>
2025-06-05 10:19:48 +0800

    Update update.fanctrlplus.php

--- include/update.fanctrlplus.php
+++ include/update.fanctrlplus.php
@@ -6,14 +6,10 @@ if (!is_dir($cfgpath)) {
   mkdir($cfgpath, 0777, true);
 }
 
-// ✅ 删除旧配置
-foreach (glob("$cfgpath/{$plugin}_*.cfg") as $f) {
-  unlink($f);
-}
+$files = $_POST['#file'] ?? [];
+foreach ($files as $index => $file) {
+  $path = "/boot/config/plugins/$plugin/" . basename($file);
 
-// ✅ 写入新配置
-$index = 0;
-while (isset($_POST["controller"][$index])) {
   $cfg = [
     'service'   => $_POST['service'][$index] ?? '0',
     'controller'=> $_POST['controller'][$index] ?? '',
@@ -23,16 +19,16 @@ while (isset($_POST["controller"][$index])) {
     'interval'  => $_POST['interval'][$index] ?? '',
     'disks'     => isset($_POST['disks'][$index]) ? implode(',', $_POST['disks'][$index]) : ''
   ];
-  $filename = "$cfgpath/{$plugin}_{$index}.cfg";
+
   $content = '';
   foreach ($cfg as $k => $v) {
     $content .= "$k=\"$v\"\n";
   }
-  file_put_contents($filename, $content);
-  $index++;
+
+  file_put_contents($path, $content);
 }
 
-// ✅ 重启风扇脚本
+// 重新启动脚本
 exec("/usr/local/emhttp/plugins/$plugin/scripts/rc.autofan stop");
 exec("/usr/local/emhttp/plugins/$plugin/scripts/rc.autofan start");
 ?>
