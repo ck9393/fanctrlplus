477f77737f6bc6c8674750f3b0f745f2729e6d0d
ck9393 <34255442+ck9393@users.noreply.github.com>
2025-06-09 09:44:53 +0800

    display errors set to 1 for diagnose html

--- include/FanctrlLogic.php
+++ include/FanctrlLogic.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 $plugin  = 'fanctrlplus';
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
